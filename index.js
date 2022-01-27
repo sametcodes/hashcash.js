8bd403e12b584c5e192cdbfa7099013054c5e349
Samet <selmansamet@pm.me>
2022-01-27 03:48:44 +0300

    Delete index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-const Hashcash = require('./hashcash')
-const hashcash = new Hashcash({resource: "sametcodes"});
-
-console.log(hashcash.computeValidHeader()) // 1:20:220127:sametcodes::MS4wMzAyNjgzMjMyNzQ4ODllKzMwOA==:MTAwMTAxMTAxMDExMTA=
-// console.log(hashcash.checkValid("1:20:220127:sametcodes::HkGGq9bK/lYPdiG7:00000001DrM")) // true
